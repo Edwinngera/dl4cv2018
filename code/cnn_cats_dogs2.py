c8cd9194be13ec416b9304ca638ef574d2e7c623
Marc Dietrichstein <marc@dietrichstein.net>
2018-12-28 11:34:19 +0100

    Standard model with augmentation

--- code/cnn_cats_dogs2.py
+++ code/cnn_cats_dogs2.py
@@ -14,26 +14,47 @@ INPUT_DIM = 3072
 NUM_CLASSES = 2
 
 
-def load_dataset(subset: Subset) -> batches.BatchGenerator:
+def load_dataset(subset: Subset, augment=False) -> batches.BatchGenerator:
     dataset = PetsDataset('../data/cifar-10-batches-py', subset)
 
-    op = ops.chain([
-        ops.hwc2chw(),
-        ops.add(-127.5),
-        ops.mul(1 / 127.5),
-        ops.type_cast(np.float32)
-    ])
+    ops_list = []
+
+    if augment:
+        ops_list += [
+            ops.hflip(),
+            ops.rcrop(32, 12, 'reflect')
+        ]
+
+    ops_list += [
+        # ops.add(-127.5),
+        ops.mul(1 / 255),
+        ops.type_cast(np.float32),
+        # Imagenet:
+        # ops.normalize(  mean=np.array([0.485, 0.456, 0.406]),
+        #                 std=np.array([0.229, 0.224, 0.225])),
+        # Cifar-10:
+        ops.normalize(  mean=np.array([0.41477802, 0.45935813, 0.49693552]),
+                        std=np.array([0.25241926, 0.24699265, 0.25279155])),
+        ops.hwc2chw()
+    ]
+
+    op = ops.chain(ops_list)
 
     return batches.BatchGenerator(dataset, 128, True, op)
 
-
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, dropout_probability=None):
         super(Net, self).__init__()
         # Deep conv filters produce better results
         # Adding more conv layers does not seem to help
-        self.conv1 = nn.Conv2d(3, 64, 5)
+        
+        self.dropout = None
+
+        if dropout_probability:
+            self.dropout = nn.Dropout(dropout_probability)
+
         self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(3, 64, 5)
         self.conv2 = nn.Conv2d(64, 128, 5)
         self.fc1 = nn.Linear(128 * 5 * 5, 128)
         self.fc2 = nn.Linear(128, 64)
@@ -44,25 +65,34 @@ class Net(nn.Module):
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, 128 * 5 * 5)
         x = F.relu(self.fc1(x))
+        if self.dropout:
+            x = self.dropout(x)
         x = F.relu(self.fc2(x))
+        if self.dropout:
+            x = self.dropout(x)
         x = self.fc3(x)
         return x
 
+def get_standard_model(dropout_probability=None):
+    model = Net(dropout_probability)
+    return model
 
 if __name__ == "__main__":
-    training_batch = load_dataset(Subset.TRAINING)
+    best_model_path = 'best_model.pth'
+
+    training_batch = load_dataset(Subset.TRAINING, augment=True)
     validation_batch = load_dataset(Subset.VALIDATION)
 
-    net = Net()
-    
+    model = get_standard_model(None)
+
     if torch.cuda.is_available():
-        net = net.cuda()
+        model = model.cuda()
 
     learning_rate = 0.001
-    # weight_decay = 0.001
     weight_decay = 0
+    # weight_decay = 0.001
 
-    cnn_cl = cnn.CnnClassifier(net, (3, 32, 32), num_classes=2, lr=learning_rate, wd=weight_decay)
+    cnn_cl = cnn.CnnClassifier(model, (3, 32, 32), num_classes=2, lr=learning_rate, wd=weight_decay, adam=False)
 
     loss_list = []
     measure = Accuracy()
@@ -70,7 +100,7 @@ if __name__ == "__main__":
     mean_loss_list = []
 
     best_accuracy = 0
-    for epoch in range(1, 301):
+    for epoch in range(1, 301):        
         predictions = np.zeros((1, 2))
         loss_list = []
         labels = []
@@ -86,7 +116,6 @@ if __name__ == "__main__":
 
         for validation_data in validation_batch:
             prediction = cnn_cl.predict(validation_data.data)
-
             predictions = np.vstack((predictions, prediction))
             labels.append(validation_data.label)
 
@@ -97,9 +126,13 @@ if __name__ == "__main__":
         measure.reset()
         measure.update(predictions, labels)
         print('\tval acc: accuracy: {0:0.3f}'.format(measure.accuracy()))
-        accuracies.append(measure.accuracy())
 
         if measure.accuracy() > best_accuracy:
             best_accuracy = measure.accuracy()
+            torch.save(model.state_dict(), best_model_path)
+            print('\tNew best accuracy. Saved model to: "{}"'.format(best_model_path))
+
+        accuracies.append(measure.accuracy())
     
     print('Best accuracy: {0:0.3f}'.format(best_accuracy))
+    
